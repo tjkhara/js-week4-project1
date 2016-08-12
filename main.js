162e417c7d606bfeedb08c9be86cce2943b23f6a
tjkhara <tkhara@gmail.com>
2016-08-11 19:15:48 -0700

    First attempt
    Tried this home work

--- main.js
+++ main.js
@@ -14,6 +14,46 @@ techniques may conflict with each other.
 
 // Add your JS here.
 
+$(document).ready(function(){
+
+// EXERCISE THE JQUERY
+
+// We'll be using the html in index.html for the following jQuery exercises.
+// After you've completed an exercise, comment out your code (unless otherwise indicated).
+
+// 1. Hide all of the paragraphs on the index.html page.
+    
+    
+//    $("p").hide();
+
+
+// 2. Hide all of the h2's on the page.
+    
+//    $("h2").hide();
+
+
+// 3. Hide all of the elements in the document.
+
+//$("#hide").hide();
+    
+    
+// 4. Hide all links on the page
+
+//    $("a").hide();
+
+// 5. Hide all of the odd table rows (Austin, Colin, & Jessica's rows)
+
+
+//  $('table tr:odd').hide();
+    
+
+// 6. Hide all paragraphs when they're clicked.
+    
+//    $("p").click(function(){
+//    $("p").hide();
+//});
+
+});
 
 
 
